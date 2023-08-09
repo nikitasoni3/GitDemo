9a08ea36d2661772ac171398a57f97d7e241b5ff
Nikita <nikitasoni8252@gmail.com>
2023-08-10 00:30:01 +0530

    First Commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Main {
+        @Test
+        public void myMethod(){
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.saucedemo.com/");
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
+        }
+    }
+
